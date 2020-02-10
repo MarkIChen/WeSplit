c08ec8f6d0e3946d3e9dbf0817e8c0a9c1a3b334
MarkStudio <markchen8024@gmail.com>
2020-02-10 21:21:15 +0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Mark Chen on 2020/2/10.
+//  Copyright © 2020 Mark Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
